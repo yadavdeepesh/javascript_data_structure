600468d99bfa2382db0ac421bc4dfff0408fd4e9
deepesh yadav <yadavdeepesh13@gmail.com>
2023-09-02 14:23:23 +0530

    array traversing in js

---
+++ JavaScript/js/array_traversing.js
@@ -0,0 +1,23 @@
+// Array Travsersing
+
+let data = [22,34,66,88,0,12,65,89,98];
+
+// for(let i = 0 ; i < data.length ; i++){
+//     document.write(data[i] + " ");
+
+// }
+
+function getValue(){
+    let value = document.getElementById("element").value;
+   
+    if(value>data.length){
+        alert(" Please enter the valid  ");
+    }
+     else{
+        document.write(data[value]);
+     }
+    // alert(value);
+    console.log(value);
+}
+
+// document.write(data[3-1]);
